049a6049787cbe8c8dbaab68f4a8a71ca9686074
OGECHI-BEN <123761516+OGECHI-BEN@users.noreply.github.com>
2023-09-17 23:28:29 +0100

    added clock assignment to my repository.

---
+++ clock/clock2.js
@@ -0,0 +1,60 @@
+// setInterval(myTime, 1000);
+// function myTime() {
+//     let time = new Date ();
+//     let hour = time.getHours();
+//     let min = time.getMinutes();
+//     let sec = time.getSeconds();
+//     am_pm = "AM";
+//     if (hour >= 12){
+//         if(hour > 12);
+//         hour -=12;
+//         am_pm ="PM";
+//     }else if (hour == 0){
+//      hour == 12;
+//      am_pm ="AM";
+//     }
+
+//     hour=
+//     hour <10 ? '0' + hour
+//     :hour;
+
+//     min =
+//     min < 10 ? '0'+ min
+//     :min;
+
+//     sec =
+//     sec < 10 ? "0" + sec +" " :sec
+// let presentTime = hour + ":" + min + ":" + sec + am_pm;
+// document.getElementById("time").innerHTML = presentTime;
+// }
+// myTime();
+
+  setInterval(myTime, 1000);
+    function myTime() {
+        let time_ = new Date();
+        let hour =time_.getHours();
+        let min = time_.getMinutes();
+        let sec = time_.getSeconds();
+        am_pm = 'AM';
+
+    if ((hour >= 12) || ( hour = 12)  ){
+       // if ( hour = 12)
+        hour -= 12;
+        am_pm = "PM";
+    } else if ( hour == 0){
+        hour == 12;
+        am_pm="AM"
+    }
+     hour=
+        hour < 10 ? "0" + hour
+        :hour;
+     min =
+         min < 10 ? "0"+ min
+         :min;
+    sec = sec < 10 ? "0" + sec :sec;
+
+    currentTime = hour + ":" + min + ':'+ sec +" "+ am_pm;
+
+     document.getElementById("time").innerHTML = currentTime;
+  }
+  myTime();
